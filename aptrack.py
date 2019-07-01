3f7cde4333f4e41a8d5baa876de113253460e8d1
Samuel Allen <samuelcallen@me.com>
2019-07-01 15:21:32 +1000

    removed 'global' variable definitions and made functions work correctly, cleaned up code as per pep8

--- aptrack.py
+++ aptrack.py
@@ -16,21 +16,21 @@ import requests
 #function for user to input a new tracking number
 #saves time having to relaunch program passing argument
 #if left out
-def in_track(track_num):
+def in_track():
     ''' input tracking number '''
     track_num = input(f'\nPlease enter your tracking number: ')
     track_num = track_num.upper()
-    if track_num is not '' and not None:
-        return track_num
-    else:
-        print(f'\nNo tracking number entered!')
-        track_num = in_track(track_num)
+    if track_num != '' and track_num is not None:
         return track_num
 
+    print(f'\nNo tracking number entered!')
+    track_num = in_track()
+    return track_num
+
 #function to display tracking information
-def show_tracking(track_response):
+def show_tracking(track_num):
     ''' show tracking number '''
-    global track_num
+    #global track_num
 
     #Actual API would use https://digitalapi.auspost.com.au/shipping/v1/track?tracking_ids=
 
@@ -44,63 +44,62 @@ def show_tracking(track_response):
     #            'Authorization: Basic NjAxYTQwMzItNmRiZC00NmFhLTljNmMtOGM2ZGFjY2E1ZTYxOnBhc3N3b3JkCg==}'})
 
     query = requests.get(url + track_num) #actual API would require requests.get(url + track_num, headers)
+
     if query.status_code == 200:
         track_response = query
         if track_num in track_response.text:
             track_response = query.json()
             print(f'\n{track_response}')
             return track_response
-        else:
-            print(f'\nTracking number not found!')
-            return track_num
-    else:
-        track_response = 'Error!'
-        print(f'\n{track_response}')
-        return track_response
 
-def menu():
+        print(f'\nTracking number not found!')
+        return track_num
+
+    track_response = 'Error!'
+    print(f'\n{track_response}')
+    return track_response
+
+def main():
     ''' show menu '''
-    global track_num
-    track_num = None
-    global track_response
-    track_response = None
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tracknum', help='run: "aptrack TRACKINGNUMBERHERE to insert tracking number"')
     #parser.add_argument('-h', '--history', help='enable history stored as TRACKINGNUMBER.txt', action='store_true')
     args = parser.parse_args()
+
     if args.tracknum is not None:
         track_num = (args.tracknum)
-        show_tracking(track_response)
-    else:
-        print(f'\nIn future, you can also run: aptrack TRACKINGNUMBERHERE to receive most recent tracking status at command line')
-        track_num = None
-        while True:
-            print(f"""
-        Active tracking number:
+        show_tracking(track_num)
+        quit()
 
-        {track_num}
+    print(f'\nIn future, you can also run: aptrack TRACKINGNUMBERHERE to receive most recent tracking status at command line')
+    track_num = None
+    while True:
+        print(f"""
+    Active tracking number:
 
-            1. Enter new tracking number
-            2. Show tracking information
-            3. Show History
-            4. Exit
-            """)
-            menu_choice = input("What would you like to do? (choose 1-4) ")
-            if menu_choice == "1":
-                track_num = in_track(track_num)
-            elif menu_choice == "2":
-                if track_num is not '' and track_num is not None:
-                    show_tracking(track_response)
-                else:
-                    print(f'\nNo tracking number present!')
-                    track_num = in_track(track_num)
-            elif menu_choice == "3":
-                track_num = 'History'
-                print("\nHistory")
-            elif menu_choice == "4":
-                print("\nGoodbye")
-                break
+    {track_num}
+
+        1. Enter new tracking number
+        2. Show tracking information
+        3. Show History
+        4. Exit
+        """)
+        menu_choice = input("What would you like to do? (choose 1-4) ")
+        if menu_choice == "1":
+            track_num = in_track()
+        elif menu_choice == "2":
+            if track_num != '' and track_num is not None:
+                show_tracking(track_num)
             else:
-                print("\nInvalid choice, please try again")
+                print(f'\nNo tracking number present!')
+                track_num = in_track()
+        elif menu_choice == "3":
+            track_num = 'History'
+            print("\nHistory")
+        elif menu_choice == "4":
+            print("\nGoodbye")
+            break
+        else:
+            print("\nInvalid choice, please try again")
 
-menu()
+main()
